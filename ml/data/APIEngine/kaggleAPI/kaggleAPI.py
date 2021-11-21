24715ae64f94a36849f3298c167694497aaf5bac
FranckEnriquez <franckenriquez63@gmail.com>
2021-11-21 15:31:30 -0600

    (feat/kaggleAPI adjusted) (autor/Francisco Enriquez)
    Kaggle API modified and tested to acquire
    data from Kaggle website.

--- ml/data/APIEngine/kaggleAPI/kaggleAPI.py
+++ ml/data/APIEngine/kaggleAPI/kaggleAPI.py
@@ -1,11 +1,16 @@
 # import external libraries
 from kaggle.api.kaggle_api_extended import KaggleApi
 
+# constants
+PATH = '../../datasets/'
+KAGGLE_PATH = 'grouplens/movielens-20m-dataset'
+
+# file names
+file_names = ['link.csv', 'genome_tags.csv']
+
+# keys authetication
 api = KaggleApi()
 api.authenticate()
 
-# Download a single file
-#Signature: dataset_download_file(dataset, file_name, path=None, force=False, quiet=True)
-PATH = '../../datasets/'
-api.dataset_download_file('grouplens/movielens-20m-dataset','link.csv', path=PATH)
-
+# download singular file
+[api.dataset_download_file(dataset=KAGGLE_PATH, file_name=i, path=PATH) for i in file_names]
