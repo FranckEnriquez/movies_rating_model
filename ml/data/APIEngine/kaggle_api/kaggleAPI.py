6ebbaa3e439f668c309358ee0249742d0f2b3ba1
FranckEnriquez <franckenriquez63@gmail.com>
2021-11-21 19:35:54 -0600

    (feat/kaggleAPI finished) (autor/Francisco Enriquez)
    kaggleAPI connected to the first step
    of ml-pipeline: EDA.

---
+++ ml/data/APIEngine/kaggle_api/kaggleAPI.py
@@ -0,0 +1,16 @@
+# import external libraries
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+# constants
+PATH = '../../datasets/'
+KAGGLE_PATH = 'grouplens/movielens-20m-dataset'
+FILES_NAMES = ['genome_tags.csv', 'genome_scores.csv',
+              'link.csv', 'movie.csv', 'rating.csv', 'tag.csv']
+
+# keys authetication
+api = KaggleApi()
+api.authenticate()
+
+# download singular file
+[api.dataset_download_file(
+    dataset=KAGGLE_PATH, file_name=i, path=PATH) for i in FILES_NAMES]
